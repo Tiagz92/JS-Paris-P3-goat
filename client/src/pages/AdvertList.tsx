efbbc37aaaab1a1c24de6ffd83e966374bfb7f4c
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2025-01-21 17:17:54 +0100

    advertList fetch creation

--- client/src/pages/AdvertList.tsx
+++ client/src/pages/AdvertList.tsx
@@ -1,31 +1,24 @@
-import type React from "react";
+import { useEffect, useState } from "react";
 import AdvertCard from "../components/AdvertCard";
-
-const adverts = [
-	{
-		id: 1,
-		title: "Annonce 1",
-		description: "Description 1",
-		goat_picture: "picture1.jpg",
-		goat_firstname: "Goat 1",
-		main_tag_name: "Tag 1",
-		sub_tag_name: "SubTag 1",
-	},
-	{
-		id: 2,
-		title: "Annonce 2",
-		description: "Description 2",
-		goat_picture: "picture2.jpg",
-		goat_firstname: "Goat 2",
-		main_tag_name: "Tag 2",
-		sub_tag_name: "SubTag 2",
-	},
-];
+import type Advert from "../types/Advert";
 
 const AdvertList: React.FC = () => {
+	const [adverts, setAdverts] = useState<Advert[]>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3001/adverts")
+			.then((res) => res.json())
+			.then((data) => {
+				setAdverts(data);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	}, []);
+
 	return (
 		<div>
-			{adverts.map((advert) => (
+			{adverts.map((advert: Advert) => (
 				<AdvertCard key={advert.id} advert={advert} />
 			))}
 		</div>
