0ab21594eead780d2d1777529295b8534816ecdf
Claire Cellier <c.cellier@gmail.com>
2025-01-22 16:53:23 +0100

    AdvertList finalised

--- client/src/pages/AdvertList.tsx
+++ client/src/pages/AdvertList.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import AdvertCard from "../components/AdvertCard";
 import type Advert from "../types/Advert";
+import "./AdvertList.css";
 
-const AdvertList: React.FC = () => {
+function AdvertList() {
 	const [adverts, setAdverts] = useState<Advert[]>([]);
 
 	useEffect(() => {
-		fetch("http://localhost:3001/adverts")
+		fetch("http://localhost:3310/api/adverts")
 			.then((res) => res.json())
 			.then((data) => {
 				setAdverts(data);
@@ -18,11 +19,13 @@ const AdvertList: React.FC = () => {
 
 	return (
 		<div>
-			{adverts.map((advert: Advert) => (
-				<AdvertCard key={advert.id} advert={advert} />
-			))}
+			<div className="adverts">
+				{adverts.map((advert: Advert) => (
+					<AdvertCard key={advert.id} advert={advert} />
+				))}
+			</div>
 		</div>
 	);
-};
+}
 
 export default AdvertList;
