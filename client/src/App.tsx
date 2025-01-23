719dcef5be9a4f040ec78f44adc28a0d19569bb5
Claire Cellier <c.cellier@gmail.com>
2025-01-23 10:46:17 +0100

    Add outlet in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-	return <h1>hello</h1>;
-}
+	return (
+	  <>
+		<Outlet />
+	  </>
+	);
+  }
 
 export default App;
