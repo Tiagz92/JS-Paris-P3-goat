c81833c49c5de1a582fcc822b39ceb3118648d22
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2025-01-23 17:18:23 +0100

    weekScheduler creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import WeekScheduler from "./components/WeekScheduler";
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<Outlet />
+			<WeekScheduler />
 		</>
 	);
 }
