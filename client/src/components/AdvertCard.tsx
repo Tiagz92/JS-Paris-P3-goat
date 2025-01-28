6025ac8f0f4dfdb981a36584e5ac81075120d822
Claire Cellier <c.cellier@gmail.com>
2025-01-28 11:50:24 +0100

    commit before merge with project updated

--- client/src/components/AdvertCard.tsx
+++ client/src/components/AdvertCard.tsx
@@ -1,5 +1,6 @@
 import type { Advert } from "../types/Advert";
 import "../components/AdvertCard.css";
+import onlineCourse from "../assets/images/onlineCourse.png";
 
 interface AdvertCardProps {
 	advert: Advert;
@@ -8,6 +9,7 @@ interface AdvertCardProps {
 function AdvertCard({ advert }: AdvertCardProps) {
 	return (
 		<div className="cards">
+			<img className="online" src={onlineCourse} alt="online-course picto by HAJICON" />
 			<img
 				className="goat-picture"
 				src={advert.goat_picture}
@@ -18,6 +20,7 @@ function AdvertCard({ advert }: AdvertCardProps) {
 				<h3 className="main-tag-name">{advert.main_tag_name}</h3>
 				<h3 className="sub-tag-name">{advert.sub_tag_name}</h3>
 			</div>
+			<p className="description">{advert.description}</p>
 			<button type="button" className="yellow-button">
 				En savoir plus
 			</button>
