7fa5ccab0f5fa8cde7bfb906c872d2314c3191f0
Claire Cellier <c.cellier@gmail.com>
2025-02-07 12:13:41 +0100

    front end done for Authentification

--- client/src/pages/ProfileLogin.tsx
+++ client/src/pages/ProfileLogin.tsx
@@ -1,19 +1,54 @@
 import { useNavigate } from "react-router-dom";
 import "./ProfileLogin.css";
+import { useRef, useState } from "react";
+import type { FormEventHandler } from "react";
 
 function Login() {
+	const emailRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
+
+	const [error, setError] = useState("");
+
+	const submitLogin: FormEventHandler = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await fetch(
+				`${import.meta.env.VITE_API_URL}/api/login`,
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						email: (emailRef.current as HTMLInputElement).value,
+						password: (passwordRef.current as HTMLInputElement).value,
+					}),
+				},
+			);
+			if (response.status === 200) navigate("/adverts");
+			else setError("Veuillez remplir tous les champs.")
+		} catch (error) {
+			console.error(error);
+			setError("Une erreur est survenue.");
+		}
+	};
+
 	const navigate = useNavigate();
 	return (
 		<div className="profilecontainer">
 			<div className="profileframe">
-				<form className="form">
+				{error && error}
+				<form className="form" onSubmit={submitLogin}>
 					<div className="emailSection">
 						<label htmlFor="email">Email :</label>
-						<input type="email" id="email" name="email" />
+						<input type="email" id="email" name="email" ref={emailRef} />
 					</div>
 					<div className="passwordSection">
 						<label htmlFor="password">Mot de passe :</label>
-						<input type="password" id="password" name="password" />
+						<input
+							type="password"
+							id="password"
+							name="password"
+							ref={passwordRef}
+						/>
 					</div>
 					<div className="submit-container">
 						<input
