b55db6b2cd6114dde9fe2092d67b60269eef29e0
Lassana88 <lassana2488@gmail.com>
2025-02-19 10:37:30 +0100

    Format JSON and TypeScript files for improved readability and consistency

--- server/src/modules/mainTag/mainTagActions.ts
+++ server/src/modules/mainTag/mainTagActions.ts
@@ -3,17 +3,17 @@ import subTagRepository from "../subTag/subTagRepository";
 import mainTagRepository from "./mainTagRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-	try {
-		const mainTags = await mainTagRepository.readAll();
+  try {
+    const mainTags = await mainTagRepository.readAll();
 
-		for (const mainTag of mainTags) {
-			mainTag.subTags = await subTagRepository.readAllByMainTag(mainTag.id);
-		}
+    for (const mainTag of mainTags) {
+      mainTag.subTags = await subTagRepository.readAllByMainTag(mainTag.id);
+    }
 
-		res.json(mainTags);
-	} catch (err) {
-		next(err);
-	}
+    res.json(mainTags);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default { browse };
--- server/src/types/slot.ts
+++ server/src/types/slot.ts
@@ -1,9 +1,9 @@
 export type Slot = {
-	id?: number;
-	advert_id: number;
-	start_at: string;
-	duration?: number;
-	meet_link?: string;
-	comment?: string;
-	goat_id?: number;
+  id?: number;
+  advert_id: number;
+  start_at: string;
+  duration?: number;
+  meet_link?: string;
+  comment?: string;
+  goat_id?: number;
 };
